a6d3462a34206f8a4d030cace43db054493b75a6
ipqw <iga538741@gmail.com>
2024-04-15 13:53:40 +0400

    add token checking

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,10 +3,12 @@ import "./_styles/globals.css";
 import "./_styles/fonts.css";
 import { Provider } from "react-redux";
 import { setupStore } from "../store/store";
+import { userApiSlice } from "@/store/services/userApiSlice";
 
 const store = setupStore();
 
 const RootLayout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
+    store.dispatch(userApiSlice.endpoints.check.initiate(localStorage.getItem("token") || ""));
     return (
         <Provider store={store}>
             <html lang="en">
--- store/reducers/userSlice.ts
+++ store/reducers/userSlice.ts
@@ -1,6 +1,7 @@
 import { IUser } from "@/app/_types/types";
 import { createSlice } from "@reduxjs/toolkit";
 import type { PayloadAction } from "@reduxjs/toolkit";
+import { userApiSlice } from "../services/userApiSlice";
 
 const initialState: IUser = {
     id: 0,
@@ -20,6 +21,20 @@ export const userSlice = createSlice({
         setUser: (state, action: PayloadAction<IUser>) => {
             state = action.payload;
         },
+    },
+    // разобраться с аунтефикацией
+    extraReducers: (builder) => {
+        builder.addMatcher(userApiSlice.endpoints.check.matchFulfilled, (state, { payload }) => {
+            if ("user" in payload) {
+                state.id = payload.user.id;
+                state.firstName = payload.user.firstName;
+                state.lastName = payload.user.lastName;
+                state.email = payload.user.email;
+                state.displayName = payload.user.displayName;
+                state.imageUrl = payload.user.imageUrl;
+                state.role = payload.user.role;
+            }
+        });
     },
 });
 
